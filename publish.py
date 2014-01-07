75a7078765a4e4707613a976720136dd273160a7
Tom McLean <tom.a.mclean@gmail.com>
2014-01-07 13:14:34 +1100

    compile all js

--- publish.py
+++ publish.py
@@ -3,12 +3,14 @@
 import jinja2
 import os
 import subprocess
+import glob
 
 if not os.path.exists("build"):
 	os.mkdir("build")
 
+scripts = [x for x in glob.glob("src/*.js") if "jquery" not in x and "gapisim" not in x]
 with open("build/script.js", "w") as f:
-	minified = subprocess.check_output(["java", "-jar", "compiler.jar", "src/script.js"])
+	minified = subprocess.check_output(["java", "-jar", "compiler.jar"] + scripts)
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="src"))
 template = env.get_template("storywar.xml")
